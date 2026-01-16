03c1980a58500669d7145bdfb919ecc6c490b420
Florian Weist <florian.weist@telekom.de>
2026-01-16 14:54:47 +0100

    feat: create first vaadin view

--- lecturer-assignment-system/src/main/java/teameins/lecturerassignmentsystem/LecturerAssignmentSystemApplication.java
+++ lecturer-assignment-system/src/main/java/teameins/lecturerassignmentsystem/LecturerAssignmentSystemApplication.java
@@ -2,12 +2,14 @@ package teameins.lecturerassignmentsystem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import com.vaadin.flow.component.dependency.StyleSheet;
+import com.vaadin.flow.component.page.AppShellConfigurator;
 
 @SpringBootApplication
-public class LecturerAssignmentSystemApplication {
+@StyleSheet("styles.css")
+public class LecturerAssignmentSystemApplication implements AppShellConfigurator {
 
     public static void main(String[] args) {
         SpringApplication.run(LecturerAssignmentSystemApplication.class, args);
     }
-
 }
