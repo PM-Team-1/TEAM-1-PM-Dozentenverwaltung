024a28d886d3b555ffc811a50550cf1f3a05fb68
Florian Weist <florian.weist@telekom.de>
2026-02-12 15:53:21 +0100

    feat: implemented necessary crud methods in CourseService

--- lecturer-assignment-system/src/main/java/teameins/lecturerassignmentsystem/service/CourseService.java
+++ lecturer-assignment-system/src/main/java/teameins/lecturerassignmentsystem/service/CourseService.java
@@ -6,7 +6,9 @@ import teameins.lecturerassignmentsystem.model.db.Course;
 import teameins.lecturerassignmentsystem.model.db.LecturerCanHoldCourse;
 import teameins.lecturerassignmentsystem.model.dto.CourseDto;
 import teameins.lecturerassignmentsystem.model.dto.LecturerCanHoldCourseDto;
+import teameins.lecturerassignmentsystem.model.exception.InvalidCourseException;
 import teameins.lecturerassignmentsystem.repository.CourseRepository;
+import teameins.lecturerassignmentsystem.repository.LecturerCanHoldCourseRepository;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -16,11 +18,48 @@ import java.util.List;
 public class CourseService {
     private final CourseRepository courseRepository;
     private final MappingService mappingService;
+    private final LecturerCanHoldCourseRepository lecturerCanHoldCourseRepository;
 
     public CourseDto getCourseById(int courseId) {
         Course course = courseRepository.findById(courseId).orElseThrow(RuntimeException::new);
         List<LecturerCanHoldCourseDto> canBeHeldBy = getLecturersWhoCanHoldCourse(courseId);
         return mappingService.map(course, canBeHeldBy);
+    }
+
+    public List<CourseDto> listCourses() {
+        List<Course> courses = courseRepository.findAll();
+        List<CourseDto> courseDtos = new ArrayList<>();
+        for (Course course : courses) {
+            List<LecturerCanHoldCourseDto> canBeHeldBy = getLecturersWhoCanHoldCourse(course.getId());
+            courseDtos.add(mappingService.map(course, canBeHeldBy));
+        }
+        return courseDtos;
+    }
+
+    public CourseDto createCourse(CourseDto courseDto) {
+        String validationResult = validateCourse(courseDto);
+        if (validationResult != null) {
+            throw new InvalidCourseException(validationResult);
+        }
+        int id = courseRepository.save(mappingService.map(courseDto)).getId();
+        return getCourseById(id);
+    }
+
+    public CourseDto updateCourse(CourseDto courseDto) {
+        String validationResult = validateCourse(courseDto);
+        if (validationResult != null) {
+            throw new InvalidCourseException(validationResult);
+        }
+        courseRepository.save(mappingService.map(courseDto));
+        return getCourseById(courseDto.getId());
+    }
+
+    public void deleteCourseById(CourseDto courseDto) {
+        for (LecturerCanHoldCourseDto lchc : courseDto.getCanBeHeldBy()) {
+            lecturerCanHoldCourseRepository.deleteById(lchc.getId());
+        }
+        courseRepository.deleteById(courseDto.getId());
+
     }
 
     private List<LecturerCanHoldCourseDto> getLecturersWhoCanHoldCourse(int courseId) {
@@ -30,5 +69,10 @@ public class CourseService {
             courseCanBeHeldyByLecturerDtoList.add(mappingService.map(lchc));
         }
         return courseCanBeHeldyByLecturerDtoList;
+    }
+
+    private String validateCourse(CourseDto courseDto) {
+        //TODO: Validation eines Kurses implementieren, wenn der Kurs invalide ist, soll der Grund zurückgegeben werden, sonst null
+        return null;
     }
 }
