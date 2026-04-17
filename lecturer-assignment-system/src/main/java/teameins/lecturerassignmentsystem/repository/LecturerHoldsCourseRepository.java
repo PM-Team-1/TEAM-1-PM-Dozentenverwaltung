df0d9a3baa5b3623e07b14e36585228f8aa9d0b6
LukasW <lukaswenzel0909@gmail.com>
2026-04-17 12:44:52 +0200

    feat: implement LecturerService and LecturerHoldsCourseRepository for managing lecturer data and course assignments

--- lecturer-assignment-system/src/main/java/teameins/lecturerassignmentsystem/repository/LecturerHoldsCourseRepository.java
+++ lecturer-assignment-system/src/main/java/teameins/lecturerassignmentsystem/repository/LecturerHoldsCourseRepository.java
@@ -3,9 +3,10 @@ package teameins.lecturerassignmentsystem.repository;
 import org.springframework.data.repository.CrudRepository;
 import teameins.lecturerassignmentsystem.model.db.LecturerHoldsCourse;
 
+import java.util.List;
 import java.util.Optional;
-
 public interface LecturerHoldsCourseRepository extends CrudRepository<LecturerHoldsCourse, Integer> {
     Optional<LecturerHoldsCourse> findByCourseId(int courseId);
     boolean existsByCourseId(int courseId);
+    List<LecturerHoldsCourse> findByLecturerId(int lecturerId);
 }
