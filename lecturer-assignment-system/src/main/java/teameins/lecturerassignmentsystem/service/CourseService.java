db7ade0c333819a39e3b90c1a7dcfa34b3f3ec42
A200367016 <luca.doerner@telekom.de>
2026-04-19 18:22:23 +0200

    reports view angefangen

--- lecturer-assignment-system/src/main/java/teameins/lecturerassignmentsystem/service/CourseService.java
+++ lecturer-assignment-system/src/main/java/teameins/lecturerassignmentsystem/service/CourseService.java
@@ -40,7 +40,12 @@ public class CourseService {
         this.lecturerRepository = lecturerRepository;
 	}
 
-	public CourseDto getCourseById(int courseId) {
+    public Course getCourseById(int courseId) {
+        return courseRepository.findById(courseId)
+                .orElseThrow(() -> new CourseNotFoundException("Es konnte keine Vorlesung mit der ID " + courseId + " gefunden werden."));
+    }
+
+	public CourseDto getCourseDtoById(int courseId) {
         Course course = courseRepository.findById(courseId)
                 .orElseThrow(() -> new CourseNotFoundException("Es konnte keine Vorlesung mit der ID " + courseId + " gefunden werden."));
         List<LecturerCanHoldCourseDto> canBeHeldBy = getLecturersWhoCanHoldCourse(courseId);
@@ -62,7 +67,7 @@ public class CourseService {
             throw new InvalidCourseException("Die Vorlesung ist ungültig.");
         }
         int id = courseRepository.save(mappingService.map(courseDto)).getId();
-        return getCourseById(id);
+        return getCourseDtoById(id);
     }
 
     public CourseDto updateCourse(CourseDto courseDto) {
@@ -72,7 +77,7 @@ public class CourseService {
         courseRepository.findById(courseDto.getId()).
                 orElseThrow(() -> new CourseNotFoundException("Es konnte keine Vorlesung mit der ID " + courseDto.getId() + " gefunden werden."));
         courseRepository.save(mappingService.map(courseDto));
-        return getCourseById(courseDto.getId());
+        return getCourseDtoById(courseDto.getId());
     }
 
     public void deleteCourse(CourseDto courseDto) {
